917ca9fb3e50cb115403a0bf6b307bdf52125db5
LeoAirmedia <erika@airmedia.com.mx>
2020-11-03 09:54:33 -0600

    - Email no unico en la validación de entrada

--- app/Http/Requests/Meetings/FreeMeetingRequest.php
+++ app/Http/Requests/Meetings/FreeMeetingRequest.php
@@ -26,7 +26,7 @@ class FreeMeetingRequest extends FormRequest
     {
         return [
             'name' => 'required|string|min:3|max:255',
-            'email' => 'required|email|unique:users,email',
+            'email' => 'required|email',
             'phone' => 'required|string|size:10|regex:/[0-9]{10}/',
             'date' => ['required', 'date', new \App\Rules\CustomDateMeeting()],
             'time' => ['required', 'string', 'regex:/^(09|(1[0-8]))\:[0-5][0-9]$/'],
