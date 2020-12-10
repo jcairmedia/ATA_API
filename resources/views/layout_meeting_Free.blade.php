423137b68f46df9bf512204d8b149025e53aab82
LeoAirmedia <erika@airmedia.com.mx>
2020-12-10 12:57:39 -0600

    Formateo del correo de cita gratuita

--- resources/views/layout_meeting_Free.blade.php
+++ resources/views/layout_meeting_Free.blade.php
@@ -1,35 +1,44 @@
-<style>
-.color-main{
-    color: #6fb258;
-}
-.text-center{
-    text-align: center
-}
-.box{
-    border:1px solid #6fb258;
-    padding: 70px;
-}
-.main{
-    padding-left: 20%;
-    padding-right: 20%;
-}
-</style>
-<div class="main">
-    <img src="" alt="">
+<!DOCTYPE html>
+<html lang="en">
 
-    <h1 class="text-center">En ATA estamos para escucharte y ayudarte en cualquier problema legal que tengas</h1>
-    <div class="box">
-        <p class="text-center">
-            Recuerda comunicarte al número <strong class="color-main"> {{$phone_office}} </strong>
-            en el día <strong>{{$day}}</strong> de <strong>{{$month}}</strong> a las <strong>{{$hours}}</strong> hrs,
-        para llevar a cabo tu guía gratuita con uno de nuestros asesores.</p>
-        <p class="text-center">
-            Nuestros asesores, esperarán un máximo de <strong>5</strong> minutos hasta dar
-            por finalizada la llamada.
-            En caso de no tomarla en la fecha y hora indicada,
-            tendrá que agendar nuevamente </p>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+
+<body style="max-width: 600px">
+    <table>
+        <tr>
+
+            <td colspan="3"><img src="http://apidev.usercenter.mx/images/header.png" alt="" /></td>
+        </tr>
+        <tr>
+            <td style="width: 180px;"></td>
+            <td style="width: 420px">
+                <p style="text-align: center; font-size:1.5em; font-weight: bold">En ATA estamos para escucharte y ayudarte en cualquier problema legal que tengas.</p>
+            </td>
+            <td></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <div style="border:1px solid #6fb258;padding: 70px;width: 458px;">
+                    <p style="text-align: center">
+                        Recuerda comunicarte al número <strong class="color-main"> {{$phone_office}} </strong>
+                        en el día <strong>{{$day}}</strong> de <strong>{{$month}}</strong> a las <strong>{{$hours}}</strong> hrs,
+                    para llevar a cabo tu guía gratuita con uno de nuestros asesores.</p>
+                    <p style="text-align: center">
+                        Nuestros asesores, esperarán un máximo de <strong>5</strong> minutos hasta dar
+                        por finalizada la llamada.
+                        En caso de no tomarla en la fecha y hora indicada,
+                        tendrá que agendar nuevamente </p>
+                </div>
+            </td>
+            <td></td>
+        </tr>
+    </table>
     </div>
-    <p class="text-center">
-        <a href="" class="color-main">Aviso de privacidad</a>
-    </p>
-</div>
+</body>
+
+</html>
