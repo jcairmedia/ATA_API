402d2a98d799b61bc211ca75340a37df7217625c
LeoAirmedia <erika@airmedia.com.mx>
2020-12-10 10:44:39 -0600

    documentacion de los endoints de cuestionario

--- app/Http/Controllers/API/TestCustomerController.php
+++ app/Http/Controllers/API/TestCustomerController.php
@@ -19,6 +19,70 @@ use App\Utils\SendEmail;
 
 class TestCustomerController extends Controller
 {
+    /**
+     * @OA\Get(
+     *      path="/api/customersatisfaction/test",
+     *      summary="Consulta de encuenta",
+     *      @OA\Parameter(in="query",
+     *           required=true,
+     *           name="uuid", required=true,
+     *           @OA\Schema(
+     *              type="string",
+     *              format="string"
+     *           )
+     *      ),
+     *      @OA\Response(
+     *        response=201,
+     *        description="Estructura de la encuesta",
+     *        @OA\JsonContent(
+     *          @OA\Property(
+     *            property="code",
+     *            type="int",
+     *            example="200"
+     *          ),
+     *          @OA\Property(
+     *            property="data",
+     *            type="array",
+     *            collectionFormat="multi",
+     *            @OA\Items(
+     *              type="object",
+     *              @OA\Property(property="questionId", type="number", example="1"),
+     *              @OA\Property(property="questionLabel", type="string"),
+     *              @OA\Property(
+     *                    property="answers",
+     *                    type="object",
+     *                    @OA\Property(property="answerId", type="number", example="10"),
+     *                    @OA\Property(property="answerLabel", type="string"),
+     *              )
+     *            )
+     *          )
+     *        )
+     *      ),
+     *      @OA\Response(
+     *        response=422,
+     *        description="uuid incorrecto",
+     *        @OA\JsonContent(
+     *          @OA\Property(
+     *            property="message",
+     *            type="string",
+     *            example="The given data was invalid."
+     *          ),
+     *          @OA\Property(
+     *            property="errors",
+     *            type="object",
+     *            @OA\Property(
+     *              property="uuid",
+     *              type="array",
+     *              collectionFormat="multi",
+     *              @OA\Items(
+     *                  type="string", example="El campo uuid seleccionado no existe."
+     *              )
+     *           ),
+     *          )
+     *        )
+     *      )
+     * )
+     */
     public function getQuestions(QuestionnaireStructCustomerRequest $request)
     {
         try {
@@ -61,6 +125,64 @@ class TestCustomerController extends Controller
         }
     }
 
+    /**
+     * @OA\Post(
+     *      path="/api/customersatisfaction/test",
+     *      summary="Registrar respuestas del cliente",
+     *      @OA\RequestBody(
+     *       required=true ,
+     *       description="Registrar una cita por pago en tienda",
+     *       @OA\JsonContent(
+     *        required={"uuid","data"},
+     *        @OA\Property(property="uuid", type="string", format="string", example="khjdkjbsjbauqinpqoq"),
+     *        @OA\Property(
+     *            property="data",
+     *            type="array",
+     *            collectionFormat="multi",
+     *            @OA\Items(
+     *                  type="object",
+     *                  @OA\Property(property="question", type="number", example="1"),
+     *                  @OA\Property(property="answer", type="number", example="1"),
+     *           ),
+     *        ),
+     *       )
+     *      ),
+     *      @OA\Response(
+     *        response=409,
+     *        description="Conflict",
+     *        @OA\JsonContent(
+     *          @OA\Property(
+     *            property="code",
+     *            type="int",
+     *            example="409"
+     *          ),
+     *          @OA\Property(
+     *            property="message",
+     *            type="string",
+     *            example="Cuestionario ya fue respondido"
+     *          )
+     *        )
+     *      ),
+     *      @OA\Response(
+     *        response=201,
+     *        description="Ok",
+     *        @OA\JsonContent(
+     *          @OA\Property(
+     *            property="code",
+     *            type="int",
+     *            example="201"
+     *          ),
+     *          @OA\Property(
+     *            property="message",
+     *            type="string",
+     *            example="Cuestionario guardado"
+     *          )
+     *        )
+     *      )
+     *    )
+     *  )
+     * )
+     */
     public function saveTest(CustomerQuestionnaireRequest $request)
     {
         try {
@@ -113,9 +235,9 @@ class TestCustomerController extends Controller
             );
             // Send email
             return response()->json([
-                'code' => 200,
+                'code' => 201,
                 'message' => 'Cuestionario guardado',
-            ], 200);
+            ], 201);
         } catch (\Exception $ex) {
             $code = (int) $ex->getCode();
             if (!(($code >= 400 && $code <= 422) || ($code >= 500 && $code <= 503))) {
