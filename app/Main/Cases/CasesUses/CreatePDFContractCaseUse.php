6028391e6e6617e33e1a5440cc8fd0aa36639ab2
LeoAirmedia <erika@airmedia.com.mx>
2020-12-10 15:53:06 -0600

    Cambio de renderizador de PDF

--- app/Main/Cases/CasesUses/CreatePDFContractCaseUse.php
+++ app/Main/Cases/CasesUses/CreatePDFContractCaseUse.php
@@ -7,9 +7,8 @@ class CreatePDFContractCaseUse
     public function __invoke($view, $uri, $path)
     {
         try {
-            $mpdf = new \Mpdf\Mpdf();
-            $mpdf->WriteHTML($view);
-            $mpdf->Output($path.$uri, 'F');
+            $pdf = App::make('dompdf.wrapper');
+            $pdf->loadHTML($view)->save($path.$uri);
         } catch (\Exception $ex) {
             \Log::error('Ocurrio un error al tratar de generar el contrato: '.print_r($ex->getMessage(), 1));
             throw new \Exception($ex->getMessage(), (int) $ex->getCode());
