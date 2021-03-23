20f958eca88980a1c0ad6d1bf3cb59ef404107b3
LeoAirmedia <erika@airmedia.com.mx>
2021-03-23 14:14:19 -0600

    proteger la entrada al end point

--- app/Http/Controllers/API/SendUsersController.php
+++ app/Http/Controllers/API/SendUsersController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SendUserRequest;
 use App\Main\Meetings\Queries\MeetingByContactsQuery;
 use App\Main\Meetings\Queries\MeetingsByUsersQuery;
-use Illuminate\Http\Request;
 
 class SendUsersController extends Controller
 {
-    public function index(Request $request)
+    public function index(SendUserRequest $request)
     {
         $m = (new MeetingByContactsQuery())();
         $u = (new MeetingsByUsersQuery())();
