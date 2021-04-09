f8d37076b53771ffa9fa894d2db3f0c73ef507bb
LeoAirmedia <erika@airmedia.com.mx>
2021-04-09 13:44:20 -0500

    Documentacion de la actualizacion de JWT de Zoom

--- app/Utils/ZoomToken.php
+++ app/Utils/ZoomToken.php
@@ -1,13 +1,13 @@
 <?php
+
 namespace App\Utils;
+
 class ZoomToken
 {
-
     public function __construct()
     {
-        $this->apikey = env("ZOOM_API_KEY");
-        $this->apiSecret = env("ZOOM_API_SECRET");
-
+        $this->apikey = env('ZOOM_API_KEY');
+        $this->apiSecret = env('ZOOM_API_SECRET');
     }
 
     public function build()
@@ -15,26 +15,24 @@ class ZoomToken
         $date = new \DateTime();
         $date->modify('+1 day');
         $day_strtotime = strtotime($date->format('Y-m-d H:i:s'));
-
+        //Generating JWTs https://marketplace.zoom.us/docs/guides/auth/jwt
         $header = [
-            "alg" => 'HS256',
-            "typ" => "JWT"
+            'alg' => 'HS256',
+            'typ' => 'JWT',
         ];
         $payload = [
-            "iss"=> $this->apikey,
-            "exp" => $day_strtotime
+            'iss' => $this->apikey,
+            'exp' => $day_strtotime,
         ];
 
         $base64header = base64_encode(json_encode($header));
         $base64payload = base64_encode(json_encode($payload));
         $response_binary = hash_hmac('SHA256',
-                                      $base64header. '.'. $base64payload,
-                                      $this->apiSecret
-                                      , true);
+                                      $base64header.'.'.$base64payload,
+                                      $this->apiSecret, true);
         $base64_signature = base64_encode($response_binary);
-        $token = $base64header. '.'. $base64payload. '.'.$base64_signature;
+        $token = $base64header.'.'.$base64payload.'.'.$base64_signature;
+
         return $token;
-
     }
-
 }
